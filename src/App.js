19773c19da0a83193b8123d2323feacbf8e8fe74
Ademola <ademolaogunmokun@yahoo.com>
2022-06-13 18:42:57 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [Person, setPerson] = useState([ { name: 'Arto Hellas' }
+]) 
+    
+  
+
+  const [newName, setNewName] = useState('')
+  return (<> 
+  
+  <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+  
+  
+  </>
   );
 }
 
